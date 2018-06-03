af0ade085c5af6231046cac91542f7460dbe8a87
Foltyn, Jakub <jakub.foltyn@sabre.com>
2018-06-03 19:30:51 +0200

    Connected frontend to backend

--- ui/src/ProductsList.js
+++ ui/src/ProductsList.js
@@ -1,46 +1,26 @@
 import React, {Component} from 'react';
 import {ProductItem} from "./ProductItem";
-
-
-let products = [
-   {
-      "id": 1,
-      "name": "Item1",
-      "description": "Description of Item1.\n\nKeywords:\nTAG1\nTAG2",
-      "category": 1
-   },
-   {
-      "id": 6,
-      "name": "Item2",
-      "description": "Description of Item.\n\nKeywords:\nTAG1\nTAG3",
-      "category": 2
-   },
-   {
-      "id": 7,
-      "name": "Item3",
-      "description": "Description of Item.\n\nKeywords:\nTAG1\nTAG2",
-      "category": 3
-   },
-   {
-      "id": 8,
-      "name": "Item4",
-      "description": "Description of Item.\n\nKeywords:\nTAG1\nTAG2",
-      "category": 4
-   },
-   {
-      "id": 9,
-      "name": "Item5",
-      "description": "Description of Item.\n\nKeywords:\nTAG4",
-      "category": 1
-   }
-];
+import axios from 'axios';
 
 class ProductsList extends Component {
+
+   state = {
+      products: []
+   };
+
+   async fetchProducts() {
+      axios.get('http://localhost:9000/products')
+              .then(res => this.setState({products: res.data}));
+   }
+
+   componentDidMount(){
+      this.fetchProducts();
+   }
 
    render() {
       return (
               <ul className="list-unstyled">
-                 {products.map(product =>
+                 {this.state.products.map(product =>
                          <ProductItem
                                  name={product.name}
                                  description={product.description}/>)}
